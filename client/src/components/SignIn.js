9635fd47825ba72cd82e99f9be85dba4c5ca58d9
engr-hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2017-07-30 00:04:53 +0600

    add react-bootstrap

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react'
+import { Button, Form, FormGroup, Col, FormControl, ControlLabel, Checkbox } from 'react-bootstrap';
 
 class SignInForm extends React.Component {
   constructor(props){
@@ -31,12 +32,39 @@ class SignInForm extends React.Component {
   render () {
     return (
       <div>
-          <form onChange={this.onFieldChanged} onSubmit={this.onSubmit}>
-            <input type="email" name="email" value={this.state.email}/>
-            <input type="password" name="password" value={this.state.password} />
-            <button type="submit">Subscribe</button>
-            {this.props.error ? <p>{ this.props.error }</p>: null}
-          </form>
+        <Form horizontal onSubmit={this.onSubmit} onChange={this.onFieldChanged}>
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={ControlLabel} sm={2}>
+              Email
+            </Col>
+            <Col sm={10}>
+              <FormControl type="email" placeholder="Email" name="email" value={this.state.email}/>
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="formHorizontalPassword">
+            <Col componentClass={ControlLabel} sm={2}>
+              Password
+            </Col>
+            <Col sm={10}>
+              <FormControl type="password" placeholder="Password" name="password" value={this.state.password}/>
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Checkbox>Remember me</Checkbox>
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button type="submit">
+                Sign In
+              </Button>
+            </Col>
+          </FormGroup>
+        </Form>
       </div>
     )
   }
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react'
+import { Button, Form, FormGroup, Col, FormControl, ControlLabel, Checkbox } from 'react-bootstrap';
 
 class SignUpForm extends React.Component {
   constructor(props){
@@ -32,13 +33,48 @@ class SignUpForm extends React.Component {
 
   render () {
     return (
-      <form onChange={this.onFieldChanged} onSubmit={this.onSubmit}>
-        <input type="text" name="name" value={this.state.name}/>
-        <input type="email" name="email" value={this.state.email}/>
-        <input type="password" name="password" value={this.state.password} />
-        <button type="submit">Subscribe</button>
-        {this.props.error ? <p>{ this.props.error }</p>: null}
-      </form>
+      // <form onChange={this.onFieldChanged} onSubmit={this.onSubmit}>
+      //   <input type="text" name="name" value={this.state.name}/>
+      //   <input type="email" name="email" value={this.state.email}/>
+      //   <input type="password" name="password" value={this.state.password} />
+      //   <button type="submit">Subscribe</button>
+      //   {this.props.error ? <p>{ this.props.error }</p>: null}
+      // </form>
+      <div>
+        <Form horizontal onSubmit={this.onSubmit} onChange={this.onFieldChanged}>
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={ControlLabel} sm={2}>
+              Email
+            </Col>
+            <Col sm={10}>
+              <FormControl type="email" placeholder="Email" name="email" value={this.state.email}/>
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="formHorizontalPassword">
+            <Col componentClass={ControlLabel} sm={2}>
+              Password
+            </Col>
+            <Col sm={10}>
+              <FormControl type="password" placeholder="Password" name="password" value={this.state.password}/>
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Checkbox>Remember me</Checkbox>
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button type="submit">
+                Sign Up
+              </Button>
+            </Col>
+          </FormGroup>
+        </Form>
+      </div>
     )
   }
 }
